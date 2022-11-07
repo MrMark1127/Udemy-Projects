a01f07363b9690aa4025758282e15fa7e71173c7
Mark <mrmark11273@gmail.com>
2022-11-07 17:39:15 -0500

    Initial Commit | Daily Journal (EJS Challenge)

---
+++ ejs-challenge/app.js
@@ -0,0 +1,96 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const { forEach, isInteger, find } = require("lodash");
+const mongoose = require("mongoose")
+const port = process.env.PORT || 3001;
+
+mongoose.connect("mongodb+srv://Mark1127:Bravesrock132@cluster0.pwkzxza.mongodb.net/blogDB")
+
+const homeStartingContent = "Hello, this is my 'blog' style website where there is a hidden route that will allow the user to compose messages, all the while saving those messages into the database setup with MongoDB. When the page is rendered, the information is requested from the database and posted to the proper route using EJS. Due to database limitations, I am unable to grant access to the composition page, but if you'd like to test the functionality of the website please feel free to shoot me an email at mark11273@gmail.com.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+let homeBody = ''
+
+const blogPostSchema = mongoose.Schema({
+  title:{
+    type: String,
+    required: [true, "The title of the post is required"]
+  },
+  body:{
+    type: String,
+    required: [true, "The body of the post is required"]
+  }
+})
+
+const Post = mongoose.model("Post", blogPostSchema)
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+//HOME ROUTE FUNCTIONS
+app.get("/", function(req,res) {
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      homeContent:homeStartingContent, 
+      postInfo:posts
+    })
+  })
+})
+
+//ABOUT ROUTE FUNCTIONS
+app.get("/about", function(req, res){
+  res.render("about", {aboutParagraph:aboutContent})
+})
+
+//CONTACT ROUTE FUNCTIONS
+app.get("/contact", function(req, res) {
+  res.render("contact", {contactParagraph:contactContent})
+})
+
+//COMPOSE ROUTE FUNCTIONS
+app.get("/compose", function(req, res){
+  res.render("compose")
+})
+
+app.post("/compose", function(req, res){
+  const postInfo = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  }
+
+  const post = new Post({
+    title: req.body.postTitle,
+    body: req.body.postBody
+  })
+
+  post.save(function(err) {
+    if (!err) {
+      res.redirect("/")
+    }
+  })
+})
+
+//POST PAGES ROUTE FUNCTIONS
+app.get("/posts/:postId", function(req, res){
+  const requestedTitle = (req.params.postId)
+  Post.findOne({_id: requestedTitle}, function(err, posts){
+    res.render(`post`, {
+      title: posts.title,
+      body: posts.body
+    })
+  })
+})
+
+app.listen(port, function() {
+  console.log("Server started on port " + port);
+});
--- todo-list-v1/app.js
+++ todo-list-v1/app.js
@@ -1,48 +1,130 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const date = require(__dirname + '/date.js')
+const mongoose = require("mongoose")
+const _ = require("lodash")
+
+const port = process.env.PORT || 3001;
 
 const app = express()
 
-const newListItems = ["Buy food", "Cook Food", "Eat Food"]
-const workItems = []
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
 
-app.get("/", function(req, res){
-    let day = date.getDate()
-    res.render('list', {listTitle: day, listItems: newListItems})
-})
+mongoose.connect("mongodb+srv://Mark1127:Bravesrock132@cluster0.pwkzxza.mongodb.net/todolistDB")
 
-app.post("/", function(req, res){
-
-    newListItem = req.body.listItem
-
-    if (req.body.list === "Work") {
-        workItems.push(newListItem)
-        res.redirect("/work")
-    } else {
-        newListItems.push(newListItem)
-        res.redirect("/")
+const itemsSchema = new mongoose.Schema ({
+    name: {
+        type: String,
+        required: [true, "Check data entry, a name is required for the item."]
     }
 })
 
-app.get("/work", function(req, res){
-    res.render('list', {listTitle: "Work List", listItems: workItems})
+const Item = mongoose.model("Item", itemsSchema)
+
+const item1 = new Item ({
+    name: "Welcome to the ToDo List"
+})
+const item2 = new Item ({
+    name: "Hit + to add a new item!"
+})
+const item3 = new Item ({
+    name: "<-- Hit this to delete an item"
 })
 
-app.post("/work", function(req, res){
-    let item = req.body.newItem
-    workItems.push(item)
-    res.redirect("/work")
+const defaultItems = [item1, item2, item3]
+
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+}
+
+const List = mongoose.model("List", listSchema)
+
+app.get("/", function(req, res){
+
+    Item.find({}, function(err, foundItems){
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, function(err) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log("Added 3 new item documents.")
+                }
+            })
+            res.redirect("/")
+        } else {
+            res.render('list', {listTitle: "Today", listItems: foundItems})
+        }
+    })    
+})
+
+app.post("/", function(req, res){
+    const newListItem = req.body.listItem
+    const listName = req.body.list
+
+    const newItem = new Item({
+        name:newListItem
+    })
+
+    if (listName === "Today") {
+        newItem.save()
+        res.redirect("/")
+    } else {
+        List.findOne({name: listName}, function(err, foundList) {
+            foundList.items.push(newItem)
+            foundList.save()
+            res.redirect(`/${listName}`)
+        })
+    }
+})
+
+app.post("/delete", function(req, res) {
+    const checkedItemID = req.body.checkbox
+    const listName = req.body.listName
+
+    if (listName === "Today") {
+        Item.findByIdAndRemove(checkedItemID, function(err){
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(`Successfully deleted ID: ${checkedItemID}`)
+            }
+        })
+        res.redirect("/")
+    } else {
+        List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemID}}}, function(err, foundList){
+            if (!err) {
+                res.redirect(`/${listName}`)
+            }
+        })
+    }
+})
+
+app.get("/:customListName", function(req,res) {
+    const customListName = _.capitalize(req.params.customListName)
+
+    List.findOne({name: customListName}, function(err, foundList) {
+        if(!err) {
+            if (!foundList) {
+                const list = new List({
+                    name: customListName,
+                    items: defaultItems
+                })
+                list.save()
+                res.redirect(`/${customListName}`)
+            } else {
+                res.render("list", {listTitle: customListName, listItems: foundList.items})
+            }
+        }
+    })
 })
 
 app.get("/about", function(req, res){
     res.render("about")
 })
 
-app.listen(3001, function(){
-    console.log("Started Server on Port 3001.")
+app.listen(port, function(){
+    console.log("Started Server on Port " + port)
 })
